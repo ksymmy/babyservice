220b0af4a8ae362ffee1708f28bf26f30cac0f8c
15805173001 <1053122492@qq.com>
2019-11-13 19:00:01 +0800

    baby控制器

---
+++ src/main/java/com/jqsoft/babyservice/controller/biz/BabyController.java
@@ -0,0 +1,20 @@
+package com.jqsoft.babyservice.controller.biz;
+
+import com.jqsoft.babyservice.controller.system.BaseController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: baby控制器
+ * @Auther: luohongbing
+ * @Date: 2019/11/13 18:59
+ */
+@Slf4j
+@RestController
+@RequestMapping("baby")
+public class BabyController extends BaseController {
+
+
+
+}
